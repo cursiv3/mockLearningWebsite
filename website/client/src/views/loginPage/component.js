3bb849d5da7791c53fbcde99961d3e4618f61cb7
Corey <lewisc503@gmail.com>
2017-09-13 20:56:21 -0700

    cors enabled

--- website/client/src/views/loginPage/component.js
+++ website/client/src/views/loginPage/component.js
@@ -3,6 +3,8 @@ import FlatButton from "material-ui/FlatButton";
 import TextField from "material-ui/TextField";
 import "./style.css";
 
+const axios = require("axios");
+
 const eventHandlers = require("./eventHandlers");
 
 class LoginPage extends Component {
@@ -15,6 +17,12 @@ class LoginPage extends Component {
     };
   }
 
+  handleSubmit = () => {
+    axios.get("http://localhost:8000/login/submit").then(data => {
+      this.setState({ user: data.data[0].username, pass: data.data[0].pword });
+    });
+  };
+
   render() {
     return (
       <div>
@@ -24,7 +32,7 @@ class LoginPage extends Component {
           <input type="text" />
           <label>Password</label>
           <input type="text" />
-          <button onClick={eventHandlers.handleSubmit}>submit</button>
+          <button onClick={e => this.handleSubmit(e)}>submit</button>
           <h3>user: {this.state.user}</h3>
           <h3>pass: {this.state.pass}</h3>
         </div>
